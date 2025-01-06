496f60b1b3b69d607d6e40be8f5bd69fb990afc6
Lovely singh <Lovely singh@LAPTOP-ICJL0B9M>
2025-01-06 22:56:29 +0530

    Sharing 3rd time

--- DecBatch/src/Forthjan_Lec/Solution2.java
+++ DecBatch/src/Forthjan_Lec/Solution2.java
@@ -6,24 +6,24 @@ package Forthjan_Lec;
 
 public class Solution2 
 {
-   int a,b,c,d,e,f;
-   public void method1(int a1,int b1,int c1,int d1,int e1)
+   int a,b,c,d;//e;
+   public void method1(int a1,int b1,int c1,int d1)
    {
 	a=a1;
 	b=b1;
 	c=c1;
 	d=d1;
-	e=e1;	  
+	//e=e1;	  
    }   
    public static void main(String[] args) 
    {
 	Solution2 obj=new Solution2();
-	obj.method1(11, 12, 13, 14, 15); // Assigning the value
+	obj.method1(11, 12, 13, 14); // Assigning the value
 	System.out.println(obj.a);    // 5 call for print the values
 	System.out.println(obj.b); 
 	System.out.println(obj.c);
 	System.out.println(obj.d);
-	System.out.println(obj.e);
+	//System.out.println(obj.e);
    }
    
 }
